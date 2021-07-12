35902a7f558ec9d8ecdf5006f7488fa1eee8b787
maksimum232@gmail.com <maksimum232@gmail.com>
2021-07-12 13:41:35 +0300

    Modifications

---
+++ README.md
@@ -0,0 +1,3 @@
+##Game Script
+Last time makefile was executed at Mon Jul 12 13:40:01 EEST 2021
+guessinggame.sh contains 24 lines of code
--- makefile.sh
+++ makefile.sh
@@ -4,6 +4,6 @@ echo "Provide the name of the Project"
 read name_var
 echo "Provide the name of the scrip"
 read name_scr
-echo "##@name_var" >> README.md
-echo "
-echo "@name_scr contains $(wc -l @name_scr | cut -c -2) lines of code"
+echo "##$name_var" > README.md
+echo "Last time makefile was executed at $(date)" >> README.md 
+echo "$name_scr contains $(wc -l $name_scr | cut -c -2) lines of code" >> README.md
